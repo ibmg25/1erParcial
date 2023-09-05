84051a040515b463b2f58831edb5e984e65e590b
Ignacio Martinez <ignacio.martinez@ucb.edu.bo>
2023-09-05 10:03:37 -0400

    Implementada funcion para contar repeticiones de una palabra en una frase

--- src/contador.js
+++ src/contador.js
@@ -3,10 +3,24 @@ export class Contador{
   aMinusculas(frase){
     return frase.toLowerCase();
   }
-}
 
-// function aMinusculas(a, b) {
-//   return a + b;
-// }
+  contarRepeticionesPalabra(palabra, frase){
+    frase = frase + '.';
+    frase = this.aMinusculas(frase);
+    let cont = 0, aux = "";
+    for(let i = 0; i < frase.length; i++){
+      if(frase[i] >= 'a' && frase[i] <= 'z'){
+        aux += frase[i];
+      }
+      else{
+        if(aux == palabra){
+          cont++;
+        }
+        aux = "";
+      }
+    }
+    return cont;
+  }
+}
 
 export default Contador;
--- src/contador.test.js
+++ src/contador.test.js
@@ -6,4 +6,9 @@ describe("Contador de Palabras", () => {
     expect(cont.aMinusculas("HOLA MunDo")).toEqual("hola mundo");
   });
 
+  it("deberia contar las repeticiones de una palabra en una frase", () => {
+    let cont = new Contador();
+    expect(cont.contarRepeticionesPalabra("hola", "HOLA MunDo hola sbshds hola hdeu hde HOLA")).toEqual(4);
+  });
+
 });
